06f8de4b92add57ae9cf6d6768353583c7c23bf2
Default <10823456@uvu.edu>
2020-09-08 14:13:38 -0600

    Upload of Project1
    Player Movement

---
+++ PlayerMovement 2/Assets/Course Library/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿// Anson Price 9/8/2020
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    private Vector3 offset = new Vector3(0, 6, -10);
+    public GameObject player;
+    void Update()
+    {
+        //set the cam to player object plus preset offset
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ PlayerMovement 2/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+    private float speed = 20f;
+    private float turnSpeed = 70f;
+    private float xInput; //Forward and back
+    private float yInput; //side to side
+
+
+    void Update()
+    {
+        yInput = Input.GetAxis("Horizontal"); //I assume it uses some list of keys like left and right arrow, along with A and D
+        xInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * xInput); // Player won't move without input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * yInput); //Tank turning!
+
+       
+    }
+}
