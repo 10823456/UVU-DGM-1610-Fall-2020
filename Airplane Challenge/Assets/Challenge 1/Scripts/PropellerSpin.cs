9ae7851beef35d6e54262c6ed9f4f334ad4b3dcf
Default <10823456@uvu.edu>
2020-09-08 13:16:46 -0600

    Upload of Unity Challenge 1 and Project 1

---
+++ Airplane Challenge/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    public float speed = 180f;
+    public GameObject propeller;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+    }
+}
